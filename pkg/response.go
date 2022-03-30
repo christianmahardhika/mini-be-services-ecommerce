a3b09199e0aac1b6aafd026bdce3b580ce5d7a38
Christian <christian.mahardhika@gmail.com>
2022-03-30 11:32:51 +0700

    add response helper

---
+++ pkg/response.go
@@ -0,0 +1,27 @@
+package response
+
+import "github.com/gin-gonic/gin"
+
+type Response struct {
+	Success    bool        `json:"success"`
+	StatusCode int         `json:"statusCode"`
+	Message    string      `json:"message"`
+	Data       interface{} `json:"data,omitempty"`
+}
+
+func Error(c *gin.Context, httpCode int, err error) {
+	c.JSON(httpCode, Response{
+		Success:    false,
+		StatusCode: httpCode,
+		Message:    err.Error(),
+	})
+}
+
+func Success(c *gin.Context, httpCode int, data interface{}) {
+	c.JSON(httpCode, Response{
+		Success:    true,
+		StatusCode: httpCode,
+		Data:       data,
+		Message:    "success",
+	})
+}
