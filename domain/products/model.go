5424f2334281637831fea4a8d5292ffa1bade473
Christian <christian.mahardhika@gmail.com>
2022-04-11 11:08:41 +0700

    fix seeder

--- domain/products/model.go
+++ domain/products/model.go
@@ -10,7 +10,7 @@ type Products struct {
 	ID        uuid.UUID `json:"id" gorm:"type:uuid;default:uuid_generate_v4();primary_key"`
 	Name      string    `json:"name" gorm:"uniqueIndex"`
 	Price     int64     `json:"price"`
-	Promo     string    `json:"promo"`
+	PromoID   uuid.UUID `json:"promoId"`
 	Stock     int64     `json:"stock"`
 	CreatedAt time.Time `json:"createdAt"`
 	UpdatedAt time.Time `json:"updatedAt"`
@@ -18,7 +18,7 @@ type Products struct {
 
 type Promo struct {
 	ID        uuid.UUID `json:"id" gorm:"type:uuid;default:uuid_generate_v4();primary_key"`
-	PromoCode string    `json:"promoCode"`
+	PromoCode string    `json:"promoCode" gorm:"uniqueIndex"`
 	PromoType string    `json:"promoType"`
 	Discount  int64     `json:"discount"`
 	CreatedAt time.Time `json:"createdAt"`
--- pkg/seeds/seeds.go
+++ pkg/seeds/seeds.go
@@ -11,12 +11,8 @@ func All() []seed.Seed {
 			Name: "Insert Products",
 			Run: func(db *gorm.DB) error {
 				var err error
-				CreateProducts(db, "Product 1", 100000, "BELI2GRATIS1", 10)
-				CreateProducts(db, "Product 2", 200000, "BELI2GRATIS1", 10)
-				CreateProducts(db, "Product 3", 300000, "BELI3GRATIS1", 10)
-				CreateProducts(db, "Product 4", 400000, "DISKON10%", 10)
-				CreateProducts(db, "Product 5", 500000, "DISKON10", 10)
-
+				CreateProducts(db, "Shampo Sunslik", 100000, "GET20OFF", "PERCENTAGE", 20, 10)
+				CreateProducts(db, "Sabun Lifeboy", 100000, "GET30OFF", "PERCENTAGE", 30, 10)
 				return err
 			},
 		},
